67fb04cbac22dd71b62464279a770ab5cb24deca
Arondit <topoohto@gmail.com>
2022-05-22 01:55:29 +0300

    add view in url for testing auth

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from api.views import CustomAuth, log_out, registration
+from api.views import CustomAuth, check_auth, log_out, registration
 
 urlpatterns = [
     path('token-auth/', CustomAuth.as_view()),
     path('register/', registration),
     path('logout/', log_out),
+    path('check-auth/', check_auth),
 ]
