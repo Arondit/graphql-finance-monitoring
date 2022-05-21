6706d1d02f3aef54c83e4408c0a4e9e36b19339e
Arondit <topoohto@gmail.com>
2022-05-22 01:39:33 +0300

    add auth

--- api/views.py
+++ api/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+import json
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
 
-# Create your views here.
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import serializers
+
+
+@api_view(['POST'])
+def registration(request):
+    body = request.body.decode('utf-8')
+    body = json.loads(body)
+    print(body)
+    username = body['username']
+    email = body['email']
+    already_registered = User.objects.filter(Q(username=username) | Q(email=email)).exists()
+
+    if already_registered:
+        raise ValueError('Already registered')
+    
+    user = User.objects.create(username=username, email=email, password=body['password'])
+
+    token, created = Token.objects.get_or_create(user=user)
+
+    return Response({'token': token.key})
+
+
+class CustomAuthSerializer(AuthTokenSerializer):
+    username = serializers.CharField(label=_("Username"), write_only=True, required=False)
+    email = serializers.CharField(label=_("email"), style={'input_type': 'password'}, required=False)
+    
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        username = attrs.get('username', '')
+        password = attrs.get('password', '')
+
+        if not email and not username or not password:
+            msg = _('Missed arguments')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        try:
+            user = User.objects.get(Q(email=email) | Q(username=username), password=password)
+        except User.DoesNotExist:
+            msg = _('Unable to log in with provided credentials.')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
+
+
+class CustomAuth(ObtainAuthToken):
+    serializer_class = CustomAuthSerializer
+
+
+def log_out(request):
+    logout(request)
