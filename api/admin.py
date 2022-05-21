b64af4b40cf333a9eb084e8d89f142e18a237c81
Arondit <topoohto@gmail.com>
2022-05-21 14:15:08 +0300

    fix models and add admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from api.models import Category, Profile, Waste
+
 # Register your models here.
+
+@admin.decorators.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Отображения профиля в панели администратора"""
+
+@admin.decorators.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Отображения категории в панели администратора"""
+
+@admin.decorators.register(Waste)
+class WasteAdmin(admin.ModelAdmin):
+    """Отображения расходов в панели администратора"""
--- api/models.py
+++ api/models.py
@@ -5,8 +5,11 @@ class Profile(models.Model):
     """Профиль пользователя"""
 
     user = models.OneToOneField('auth.User', verbose_name='Пользователь-владелец профиля', on_delete=models.CASCADE)
-    bill = models.DecimalField('Счет пользователя в рублях', decimal_places=2, default=0, max_digits=2)
+    bill = models.DecimalField('Счет пользователя в рублях', decimal_places=2, default=0, max_digits=20)
     name = models.CharField('Имя пользователя', max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
@@ -23,15 +26,21 @@ class Category(models.Model):
         null=True, 
         default=None, 
         decimal_places=2, 
-        max_digits=2,
+        max_digits=20,
     )
     date_created = models.DateField('Дата создания категории', auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Waste(models.Model):
     """Трата"""
 
     name = models.CharField('На что были потрачены деньги', max_length=200)
-    amount = models.DecimalField('Количество потраченных денег', decimal_places=2, max_digits=2)
+    amount = models.DecimalField('Количество потраченных денег', decimal_places=2, max_digits=20)
     category = models.ForeignKey('api.Category', verbose_name='Категория расходов', on_delete=models.PROTECT)
     datetime_created = models.DateTimeField('Время траты денег', auto_now=True)
+
+    def __str__(self):
+        return self.name
