90bd58f0a11a3173b20b8155fd2b6639e7b28cca
Arondit <topoohto@gmail.com>
2022-05-22 22:37:07 +0300

    remove security header for cors

--- finance_monitoring/settings.py
+++ finance_monitoring/settings.py
@@ -61,8 +61,6 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
